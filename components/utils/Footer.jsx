4af54c6cfb7cf2fbb75e449a837cc995fcc74190
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-08-02 19:52:15 +0100

    first commit.

---
+++ components/utils/Footer.jsx
@@ -0,0 +1,50 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { Link, Typography } from "@material-ui/core";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link target="blank_" color="inherit" href="https://ahmedbargady.me">
+        Jina Cool Projects
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: theme.spacing(6),
+    marginTop: 24,
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
+  },
+}));
+function Footer() {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <Typography
+        color="textSecondary"
+        variant="h6"
+        align="center"
+        gutterBottom>
+        Created with ❤️ By:
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="textSecondary"
+        component="p">
+        Ahmed BARGADY
+      </Typography>
+      <Copyright />
+    </footer>
+  );
+}
+
+export default Footer;
